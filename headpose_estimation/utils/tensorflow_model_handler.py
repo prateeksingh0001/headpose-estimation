6774b65b2e433d5660ac48f4e573c6191414b2c1
prateeksingh0001 <prateeksingh0001@gmail.com>
2026-05-10 20:54:41 -0400

    Added more refactors based on review of the code

--- headpose_estimation/utils/tensorflow_model_handler.py
+++ headpose_estimation/utils/tensorflow_model_handler.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
+import os
 from functools import partial
 import logging
 from pathlib import Path
@@ -7,7 +8,6 @@ import tarfile
 from typing import List, Tuple, Dict, Union
 from urllib import request
 import yaml
-from yaml import Loader
 
 
 logging.basicConfig(level = logging.INFO)
@@ -39,47 +39,47 @@ class TensorflowV1ModelHandler:
     """
     Handler for TF1 models. Given a model name it performs the following functions:
     - Download the model if not present on disk
-    - Provide information about the model, for more information check out the `ModelConfig` class.
-
+    - Provide information about the model, for more information check out the `TensorflowV1ModelConfig` class.
     """
+
     def __init__(
         self,
         tf_models_config_path: str
     ):
         with open(tf_models_config_path) as f:
-            models_list: List[Dict[str, Union[str, int, Tuple[int, int]]]] = yaml.load(f, Loader=Loader)
+            models_list: List[Dict[str, Union[str, int, Tuple[int, int]]]] = yaml.safe_load(f)
 
         self.models = {
             model["architecture"]: TensorflowV1ModelConfig.from_dict(model) for model in models_list
         }
 
-    def get_model_info(self, architecture: str) -> Dict[str, Union[str, int, Tuple[int, int]]]:
+    def get_model_info(self, architecture: str) -> TensorflowV1ModelConfig:
         if architecture not in self.models:
             raise ValueError(
                 f"{architecture} not specified in the tf model configs, check the architecture name or update the model configs"
             )
+        return self.models[architecture]
 
-        return asdict(self.models.get(architecture))
-
-    def get_model(self, architecture: str, output_path: str) -> str:
-        model_path = Path(output_path) / architecture
+    def get_model(self, architecture: str, output_path: str) -> Path:
+        model_folder = Path(output_path)
+        model_path = model_folder / architecture
         if model_path.exists():
             return model_path
 
-        logger.info("Downloading model %s"%(architecture))
-        downloaded_model_path = self._download_model(architecture, output_path)
-        model_dir = self._extract_model_file(downloaded_model_path, output_path)
+        logger.info("Downloading model %s", architecture)
+        downloaded_model_path = self._download_model(architecture, model_folder)
+        self._extract_model_file(downloaded_model_path, model_folder)
 
-        return model_dir
+        return model_path
 
     @staticmethod
     def __model_download_progress_bar(count: int, block_size: float, total_size: float, filename: str) -> None:
-        logger.info("Downloaded model %s %0.2f%%"%(filename, float((count*block_size*100)/total_size)))
+        logger.info("Downloaded model %s %0.2f%%", filename, float((count*block_size*100)/total_size))
 
-    def _download_model(self, architecture: str, output_path: str) -> str:
-        download_url = self.models.get(architecture).download_url
+    def _download_model(self, architecture: str, output_path: Path) -> Path:
+        download_url = self.models[architecture].download_url
         model_tarfile = download_url.split("/")[-1]
-        downloaded_model_filepath = Path(output_path).joinpath(model_tarfile)
+        downloaded_model_filepath = output_path / model_tarfile
 
         request.urlretrieve(
             url=download_url,
@@ -90,9 +90,10 @@ class TensorflowV1ModelHandler:
         return downloaded_model_filepath
 
     @staticmethod
-    def _extract_model_file(model_path: str, unzip_dir: str) -> str:
-        logger.info("Extracting %s into %s"%(model_path, unzip_dir))
-        with tarfile.open(model_path, "r:gz") as tar_file:
-            tar_file.extractall(unzip_dir)
+    def _extract_model_file(model_path: Path, unzip_dir: Path) -> None:
+        logger.info("Extracting %s into %s", model_path, unzip_dir)
 
-        return unzip_dir
+        with tarfile.open(model_path, "r:gz") as tar_file:
+            tar_file.extractall(unzip_dir, filter="tar")
+        
+        os.remove(model_path)
