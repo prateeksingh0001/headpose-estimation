6b63d4de0b32c9eb850d1a56e256faedce525170
prateeksingh0001 <prateeksingh0001@gmail.com>
2026-05-31 02:59:02 -0400

    Added constants being used across the codebase

---
+++ headpose_estimation/utils/constants.py
@@ -0,0 +1,6 @@
+TF_MODELS_CONFIG_PATH = "configs/tf_models/tf_models.yaml"
+DEFAULT_SAVE_NP_ARRAY_NAME = "arr_0"
+
+# Tensorflow related constants
+TENSORBOARD_DIR_NAME = "tensorboard_logs"
+DEFAULT_PREDICTION_HEAD_FROZEN_GRAPH_NAME = "prediction_head.pb"
