ad9259563039eeb852fd8f19a26dcf1821d8f979
prateeksingh0001 <prateeksingh0001@gmail.com>
2026-06-06 19:16:48 -0400

    Added dataset preprocessor for the 300WLP dataset and updated the scripts to subsample each dataset

--- headpose_estimation/dataset_preprocessors.py
+++ headpose_estimation/dataset_preprocessors.py
@@ -12,13 +12,19 @@ Steps:
 """
 
 import logging
+import math
 import os
 import sys
+from abc import abstractmethod
 from pathlib import Path
 from typing import Dict, List, Optional, Union
 
 import cv2
+import numpy as np
 import pandas as pd
+from scipy.io import loadmat
+
+from headpose_estimation.utils.constants import GROUND_TRUTH_FILE_NAME, IMAGE_FOLDER_NAME
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -28,8 +34,21 @@ formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
+ProcessedFrameType = List[Dict[str, Union[str, float]]]
 
-class UPNAPreprocessor:
+
+class BaseDatasetPreprocessor:
+  def __init__(self, dataset_name: str, dataset_path: str, output_path: str):
+    self._dataset_name = dataset_name
+    self._dataset_path = Path(dataset_path)
+    self._images_path = Path(output_path)
+
+  @abstractmethod
+  def process_dataset(self, sample_size: Optional[int] = None) -> ProcessedFrameType:
+    pass
+
+
+class UPNAPreprocessor(BaseDatasetPreprocessor):
   """
   The UPNA dataset consists of mp4 videos each of 300 frames and the ground truth values, in degrees, for each frame.
   the ground truth files(user_<n>_video_<k>_groundtruth3D.txt) files contain 6 columns contain the
@@ -57,19 +76,13 @@ class UPNAPreprocessor:
     "pitch",
   ]
 
-  def __init__(self, dataset_name: str, dataset_path: str, output_path: str):
-    self._dataset_name = dataset_name
-    self._dataset_path = Path(dataset_path)
-    self._output_path = Path(output_path)
-    self._images_path = Path(output_path)
-
   def process_single_video(
     self,
     video_path: Path,
     ground_truth: pd.DataFrame,
     running_sample_size: int,
     total_sample_size: Optional[int] = None,
-  ) -> List[Dict[str, Union[str, float]]]:
+  ) -> ProcessedFrameType:
 
     video_capture = cv2.VideoCapture(video_path)
 
@@ -110,13 +123,13 @@ class UPNAPreprocessor:
     video_capture.release()
     cv2.destroyAllWindows()
 
-    return processed_frames
+    return processed_frames, running_sample_size
 
   def process_videos_for_username(
     self, username_path: Path, running_sample_size: int, total_sample_size: Optional[int] = None
-  ) -> List[Dict[str, Union[str, float]]]:
+  ) -> ProcessedFrameType:
 
-    processed_frames: List[Dict[str, Union[str, float]]] = []
+    processed_frames: ProcessedFrameType = []
     for video_path in username_path.glob("*.mp4"):
       logger.info(f"Processing file {video_path}")
 
@@ -128,37 +141,101 @@ class UPNAPreprocessor:
         names=self.GROUND_TRUTH_FILE_HEADERS,
         index_col=False,
       )
-      processed_frames.extend(
-        self.process_single_video(
-          video_path=video_path,
-          ground_truth=ground_truth,
-          running_sample_size=running_sample_size,
-          total_sample_size=total_sample_size,
-        )
+      single_video_processed_frames, running_sample_size = self.process_single_video(
+        video_path=video_path,
+        ground_truth=ground_truth,
+        running_sample_size=running_sample_size,
+        total_sample_size=total_sample_size,
       )
 
-      if running_sample_size == total_sample_size:
+      processed_frames.extend(single_video_processed_frames)
+
+      if total_sample_size and running_sample_size == total_sample_size:
         break
 
-    return processed_frames
+    return processed_frames, running_sample_size
 
-  def process_dataset(self, sample_size: Optional[int] = None) -> List[Dict[str, Union[str, float]]]:
+  def process_dataset(self, sample_size: Optional[int] = None) -> ProcessedFrameType:
 
-    if not os.path.exists(self._images_path):
-      self._images_path.mkdir(exist_ok=True)
-
-    processed_frames: List[Dict[str, Union[str, float]]] = []
+    processed_frames: ProcessedFrameType = []
     running_sample_size = 0
     for username in os.listdir(self._dataset_path):
-      processed_frames.extend(
-        self.process_videos_for_username(
-          username_path=self._images_path / username,
-          running_sample_size=running_sample_size,
-          total_sample_size=sample_size,
-        )
+      user_processed_frames, running_sample_size = self.process_videos_for_username(
+        username_path=self._dataset_path / username,
+        running_sample_size=running_sample_size,
+        total_sample_size=sample_size,
       )
 
-      if running_sample_size == sample_size:
+      processed_frames.extend(user_processed_frames)
+
+      if sample_size and running_sample_size == sample_size:
         break
 
     return processed_frames
+
+
+class ThreeHudredWLPPreprocessor(BaseDatasetPreprocessor):
+  """
+  Preprocessor for the 300WLP dataset. The dataset consists of multiple subdatasets list in the class variable
+  `SUB_DATASET_NAMES`.
+  Each subdataset consists of multiple image and a matlab data files(.mat) associated with each image, each matlab
+  file consists of the following keys:
+  - pt2d
+  - roi
+  - Illum_Para
+  - Color_Para
+  - Tex_Para
+  - Shape_Para
+  - Exp_Para
+  - Pose_Para
+
+  The Pose_Para key contains 6 values the first three of which are Pitch, Yaw and Roll
+  """
+
+  SUB_DATASET_NAMES = ["AFW", "AFW_Flip", "HELEN", "HELEN_Flip", "IBUG", "IBUG_Flip", "LFPW", "LFPW_Flip"]
+
+  def process_sub_dataset(
+    self, sub_dataset_path: Path, running_sample_size: int, sample_size: Optional[int] = None
+  ) -> ProcessedFrameType:
+    processed_frames: ProcessedFrameType = []
+    for image_path in sub_dataset_path.glob("*.jpg"):
+      image_file_name = image_path.stem
+      image_data = loadmat(image_path.with_suffix(".mat"))
+
+      # Convert radians to degrees
+      pitch, yaw, roll = map(lambda x: math.degrees(x), image_data["Pose_Para"][0, :3])
+
+      processed_frames.append(
+        {
+          "id": str(image_file_name),
+          "image_path": str(image_path),
+          "yaw_ground_truth": yaw,
+          "pitch_ground_truth": pitch,
+          "roll_ground_truth": roll,
+        }
+      )
+
+      running_sample_size += 1
+
+      if sample_size and running_sample_size == sample_size:
+        break
+
+    return processed_frames, running_sample_size
+
+  def process_dataset(self, sample_size: Optional[int] = None) -> ProcessedFrameType:
+    running_sample_size = 0
+    processed_dataset: ProcessedFrameType = []
+    for sub_dataset_name in self.SUB_DATASET_NAMES:
+      sub_dataset_path = self._dataset_path / sub_dataset_name
+      processed_sub_dataset, running_sample_size = self.process_sub_dataset(
+        sub_dataset_path=sub_dataset_path,
+        running_sample_size=running_sample_size,
+        sample_size=sample_size,
+      )
+
+      processed_dataset.extend(processed_sub_dataset)
+
+      if sample_size and running_sample_size == sample_size:
+        break
+
+    return processed_dataset
--- scripts/python/run_dataset_preprocessing.py
+++ scripts/python/run_dataset_preprocessing.py
@@ -3,12 +3,12 @@ from argparse import ArgumentParser
 from pathlib import Path
 from typing import Any, Dict, Union
 
-from headpose_estimation.dataset_preprocessors import UPNAPreprocessor
-from headpose_estimation.schema import DataPreprocessingConfig, SingleDatasetPreprocessingConfig
+from headpose_estimation.dataset_preprocessors import ProcessedFrameType, ThreeHudredWLPPreprocessor, UPNAPreprocessor
+from headpose_estimation.schema import DataPreprocessingConfig
 from headpose_estimation.utils.constants import GROUND_TRUTH_FILE_NAME, IMAGE_FOLDER_NAME
 from headpose_estimation.utils.utils import cls_from_str
 
-DataPreprocessorType = Union[UPNAPreprocessor]
+DataPreprocessorType = Union[UPNAPreprocessor, ThreeHudredWLPPreprocessor]
 
 
 def main() -> None:
@@ -25,16 +25,16 @@ def main() -> None:
   if not image_folder_path.exists():
     image_folder_path.mkdir(exist_ok=True)
 
-  output_frame_information = []
+  output_frame_information: ProcessedFrameType = []
   for dataset in preprocessing_config.datasets:
     dataset_preprocessor: DataPreprocessorType = cls_from_str(dataset.preprocessor_class)(
       dataset_name=dataset.dataset_name,
       dataset_path=preprocessing_config.dataset_root_path / dataset.dataset_name,
       output_path=image_folder_path,
     )
-    output_frame_information.extend(dataset_preprocessor.process_dataset())
+    output_frame_information.extend(dataset_preprocessor.process_dataset(sample_size=dataset.subsample_size))
 
-  with open(preprocessing_config.output_path / GROUND_TRUTH_FILE_NAME) as f:
+  with open(preprocessing_config.output_path / GROUND_TRUTH_FILE_NAME, "w") as f:
     json.dump(output_frame_information, f)
 
 
